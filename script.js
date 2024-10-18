b4ef819bd054887650c16094787a2070a26cedfc
fiqielz <afiq64070@gmail.com>
2024-10-18 15:55:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.getElementById('title').addEventListener('click', function() {
     window.location.href = 'second.html'; // Bawa ke next screen
 });
+
+function customizeName(section) {
+    const newName = prompt("Enter new name:", section.textContent);
+    if (newName) {
+        section.textContent = newName; // Update section dengan nama baru
+    }
+}
